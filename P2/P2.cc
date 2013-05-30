c99601224c00ba27bea23a3de2e0c52542729b07
Lars Immisch <lars@ibp.de>
2013-05-30 09:59:37 +0200

    Remove unused reverse

--- P2/P2.cc
+++ P2/P2.cc
@@ -77,13 +77,6 @@ public:
         transfer(state);
     }
 
-    static byte reverse(byte b) {
-        b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-        b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-        b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-        return b;
-    }
-
     static void set(byte b, bool on) {
         byte v = 1 << b;
 
